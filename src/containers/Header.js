bf4bf09aa72de923607aa723e22162fbf051db57
Fatya0802 <fatya.gasanova2002@gmail.com>
2022-12-28 16:36:47 +0400

    _2

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 const Header = () => {
-    return (
+  return (
     <div className="ui fixed menu">
-        <div className="ui container center">
-            <h2>Miracle</h2>
-        </div>
+      <div className="ui container center">
+        <h2>Miracle</h2>
+      </div>
     </div>
-    );
+  );
 };
 
 export default Header;
